de526b73f75b71009966362189dc299854380862
Elisabet <dormaulisimanjuntak@hotmail.com>
2024-04-02 21:02:43 +0700

    edit_tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,17 +1,17 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./tailwind/*.{html,js}"],
+  content: ["./test_tailwind/*.{html}"],
   theme: {
-    container: {
-      center: true,
-    },
-    extend: {
-      colors: {
-        dark: "#0f172a",
-        primary: "#14b8a6",
-        ham: "#F9FAFB",
-      },
-    },
+    // container: {
+    //   center: true,
+    // },
+    // extend: {
+    //   colors: {
+    //     dark: "#0f172a",
+    //     primary: "#14b8a6",
+    //     ham: "#F9FAFB",
+    //   },
+    // },
   },
   plugins: [],
 };
